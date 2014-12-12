6da3b04e2d2cb1fb04fd6e3288ed8c2942ba4f94
Remi <remi.moinas@gmail.com>
2014-12-12 11:24:27 +0100

    Add ping and keepAlive function in connection instance Add action to pong response Add Promise require in composer.json

--- src/Ratchet/WebSocket/Version/RFC6455.php
+++ src/Ratchet/WebSocket/Version/RFC6455.php
@@ -1,5 +1,6 @@
 <?php
 namespace Ratchet\WebSocket\Version;
+
 use Ratchet\ConnectionInterface;
 use Ratchet\MessageInterface;
 use Ratchet\WebSocket\Version\RFC6455\HandshakeVerifier;
@@ -16,7 +17,8 @@ use Guzzle\Http\Message\Response;
  * @link http://tools.ietf.org/html/rfc6455
  * @todo Unicode: return mb_convert_encoding(pack("N",$u), mb_internal_encoding(), 'UCS-4BE');
  */
-class RFC6455 implements VersionInterface {
+class RFC6455 implements VersionInterface
+{
     const GUID = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
 
     /**
@@ -35,7 +37,8 @@ class RFC6455 implements VersionInterface {
      */
     protected $validator;
 
-    public function __construct(ValidatorInterface $validator = null) {
+    public function __construct(ValidatorInterface $validator = null)
+    {
         $this->_verifier = new HandshakeVerifier;
         $this->setCloseCodes();
 
@@ -49,7 +52,8 @@ class RFC6455 implements VersionInterface {
     /**
      * {@inheritdoc}
      */
-    public function isProtocol(RequestInterface $request) {
+    public function isProtocol(RequestInterface $request)
+    {
         $version = (int)(string)$request->getHeader('Sec-WebSocket-Version');
 
         return ($this->getVersionNumber() === $version);
@@ -58,31 +62,34 @@ class RFC6455 implements VersionInterface {
     /**
      * {@inheritdoc}
      */
-    public function getVersionNumber() {
+    public function getVersionNumber()
+    {
         return 13;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function handshake(RequestInterface $request) {
+    public function handshake(RequestInterface $request)
+    {
         if (true !== $this->_verifier->verifyAll($request)) {
             return new Response(400);
         }
 
         return new Response(101, array(
-            'Upgrade'              => 'websocket'
-          , 'Connection'           => 'Upgrade'
-          , 'Sec-WebSocket-Accept' => $this->sign((string)$request->getHeader('Sec-WebSocket-Key'))
+            'Upgrade' => 'websocket'
+        , 'Connection' => 'Upgrade'
+        , 'Sec-WebSocket-Accept' => $this->sign((string)$request->getHeader('Sec-WebSocket-Key'))
         ));
     }
 
     /**
      * @param  \Ratchet\ConnectionInterface $conn
-     * @param  \Ratchet\MessageInterface    $coalescedCallback
+     * @param  \Ratchet\MessageInterface $coalescedCallback
      * @return \Ratchet\WebSocket\Version\RFC6455\Connection
      */
-    public function upgradeConnection(ConnectionInterface $conn, MessageInterface $coalescedCallback) {
+    public function upgradeConnection(ConnectionInterface $conn, MessageInterface $coalescedCallback)
+    {
         $upgraded = new Connection($conn);
 
         if (!isset($upgraded->WebSocket)) {
@@ -96,9 +103,10 @@ class RFC6455 implements VersionInterface {
 
     /**
      * @param \Ratchet\WebSocket\Version\RFC6455\Connection $from
-     * @param string                                        $data
+     * @param string $data
      */
-    public function onMessage(ConnectionInterface $from, $data) {
+    public function onMessage(ConnectionInterface $from, $data)
+    {
         $overflow = '';
 
         if (!isset($from->WebSocket->message)) {
@@ -155,15 +163,23 @@ class RFC6455 implements VersionInterface {
                         }
 
                         return $from->close($frame);
-                    break;
+                        break;
                     case $frame::OP_PING:
                         $from->send($this->newFrame($frame->getPayload(), true, $frame::OP_PONG));
-                    break;
+                        break;
                     case $frame::OP_PONG:
-                    break;
+                        $uniqId = $frame->getPayload();
+                        if ($from->WebSocket->pingH[$uniqId]->deferredPong !== null) {
+                            $from->WebSocket->pingH[$uniqId]->pongTimer->cancel();
+                            $pongResponseDiff = (new \DateTime())->getTimestamp() - $from->WebSocket->pingH[$uniqId]->lastPingTimestamp;
+                            $deferred = $from->WebSocket->pingH[$uniqId]->deferredPong;
+                            unset($from->WebSocket->pingH[$uniqId]);
+                            $deferred->resolve($pongResponseDiff);
+                        }
+                        break;
                     default:
                         return $from->close($frame::CLOSE_PROTOCOL);
-                    break;
+                        break;
                 }
 
                 $overflow = $from->WebSocket->frame->extractOverflow();
@@ -210,17 +226,19 @@ class RFC6455 implements VersionInterface {
     /**
      * @return RFC6455\Message
      */
-    public function newMessage() {
+    public function newMessage()
+    {
         return new Message;
     }
 
     /**
      * @param string|null $payload
-     * @param bool|null   $final
-     * @param int|null    $opcode
+     * @param bool|null $final
+     * @param int|null $opcode
      * @return RFC6455\Frame
      */
-    public function newFrame($payload = null, $final = null, $opcode = null) {
+    public function newFrame($payload = null, $final = null, $opcode = null)
+    {
         return new Frame($payload, $final, $opcode);
     }
 
@@ -230,7 +248,8 @@ class RFC6455 implements VersionInterface {
      * @return string
      * @internal
      */
-    public function sign($key) {
+    public function sign($key)
+    {
         return base64_encode(sha1($key . static::GUID, true));
     }
 
@@ -239,7 +258,8 @@ class RFC6455 implements VersionInterface {
      * @param int|string
      * @return bool
      */
-    public function isValidCloseCode($val) {
+    public function isValidCloseCode($val)
+    {
         if (array_key_exists($val, $this->closeCodes)) {
             return true;
         }
@@ -254,18 +274,19 @@ class RFC6455 implements VersionInterface {
     /**
      * Creates a private lookup of valid, private close codes
      */
-    protected function setCloseCodes() {
-        $this->closeCodes[Frame::CLOSE_NORMAL]      = true;
-        $this->closeCodes[Frame::CLOSE_GOING_AWAY]  = true;
-        $this->closeCodes[Frame::CLOSE_PROTOCOL]    = true;
-        $this->closeCodes[Frame::CLOSE_BAD_DATA]    = true;
+    protected function setCloseCodes()
+    {
+        $this->closeCodes[Frame::CLOSE_NORMAL] = true;
+        $this->closeCodes[Frame::CLOSE_GOING_AWAY] = true;
+        $this->closeCodes[Frame::CLOSE_PROTOCOL] = true;
+        $this->closeCodes[Frame::CLOSE_BAD_DATA] = true;
         //$this->closeCodes[Frame::CLOSE_NO_STATUS]   = true;
         //$this->closeCodes[Frame::CLOSE_ABNORMAL]    = true;
         $this->closeCodes[Frame::CLOSE_BAD_PAYLOAD] = true;
-        $this->closeCodes[Frame::CLOSE_POLICY]      = true;
-        $this->closeCodes[Frame::CLOSE_TOO_BIG]     = true;
-        $this->closeCodes[Frame::CLOSE_MAND_EXT]    = true;
-        $this->closeCodes[Frame::CLOSE_SRV_ERR]     = true;
+        $this->closeCodes[Frame::CLOSE_POLICY] = true;
+        $this->closeCodes[Frame::CLOSE_TOO_BIG] = true;
+        $this->closeCodes[Frame::CLOSE_MAND_EXT] = true;
+        $this->closeCodes[Frame::CLOSE_SRV_ERR] = true;
         //$this->closeCodes[Frame::CLOSE_TLS]         = true;
     }
 }
